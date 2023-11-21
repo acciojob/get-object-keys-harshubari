f32967e93bc1fa0a159dafede33853ab3180dd02
harshubari <121845828+harshubari@users.noreply.github.com>
2023-11-21 06:55:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const student = {
+  name: 'John Doe',
+};
+
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+const keys = student.getKeys();
+console.log(keys); 
